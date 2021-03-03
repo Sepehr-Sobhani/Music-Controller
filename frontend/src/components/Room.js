b503996f6689c6f62de142ed2603285e60c0ad8a
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2021-03-02 20:02:01 -0800

    Add leave room button functionality and view

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Grid, Button, Typography } from "@material-ui/core";
-import { Link } from "react-router-dom";
 export default class Room extends Component {
   constructor(props) {
     super(props);
@@ -11,11 +10,18 @@ export default class Room extends Component {
     };
     this.roomCode = this.props.match.params.roomCode;
     this.getRoomDetails();
+    this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
   }
 
   getRoomDetails() {
     fetch("/api/get-room" + "?code=" + this.roomCode)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          this.props.leaveRoomCallback();
+          this.props.history.push("/");
+        }
+        return response.json();
+      })
       .then((data) => {
         this.setState({
           votesToSkip: data.votes_to_skip,
@@ -23,6 +29,17 @@ export default class Room extends Component {
           isHost: data.is_host,
         });
       });
+  }
+
+  leaveButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/api/leave-room", requestOptions).then((_response) => {
+      this.props.leaveRoomCallback();
+      this.props.history.push("/");
+    });
   }
 
   render() {
@@ -49,7 +66,11 @@ export default class Room extends Component {
           </Typography>
         </Grid>
         <Grid item xs={12} align="center">
-          <Button variant="contained" color="secondary" to="/" component={Link}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={this.leaveButtonPressed}
+          >
             Leave Room
           </Button>
         </Grid>

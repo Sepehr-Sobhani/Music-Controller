17d0540b7cf305824cb743d687f48ae4c16c11d2
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2021-03-05 23:46:57 -0800

    User authentication with spotify API

--- spotify/views.py
+++ spotify/views.py
@@ -1,10 +1,11 @@
 from django.http import response
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .credentials import REDIRECT_URI, CLIENT_ID, CLIENT_SECRET
 from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
+from .util import is_spotify_authenticated, update_or_create_user_tokens
 
 
 class AuthURL(APIView):
@@ -38,3 +39,18 @@ def spotify_callback(request, format=None):
     refresh_token = response.get('refresh_token')
     expires_in = response.get('expires_in')
     error = response.get('error')
+
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
+
+    update_or_create_user_tokens(
+        request.session.session_key, access_token, token_type, expires_in, refresh_token)
+
+    return redirect('frontend:')
+
+
+class IsAuthenticated(APIView):
+    def get(self, request, format=None):
+        is_authenticated = is_spotify_authenticated(
+            self.request.session.session_key)
+        return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
